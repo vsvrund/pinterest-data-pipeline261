a1b8a0a9a8ada7c449921ffad3515f2d5fc033a9
vsvrund <svrund2000@yahoo.com>
2024-01-14 14:28:08 +0000

    Fix dag_id to avoid AWS permission error.

---
+++ 0a1e5630c127_dag.py
@@ -0,0 +1,48 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': '/Users/svrund2000@yahoo.com/databricks_batch_processing',
+}
+
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': 'vrund',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG(
+    dag_id='0a1e5630c127_dag',
+    # should be a datetime format
+    start_date=datetime(2024, 1, 14),
+    # check out possible intervals, should be a string
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
+
+print(notebook_task)
